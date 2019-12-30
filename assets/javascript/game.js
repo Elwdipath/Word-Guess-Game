eb19045c6a052bcd15edf5ebce90e089a6a7b5c5
Jordan Kaiser <jordan.kaiser@gmail.com>
2019-12-30 12:50:28 -0500

    Pseudo-coded and created game object

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//Theme is Top Gun
+// create game object that contains words to be guessed, letters already guessed, wins, number of guesses remaining. 
+var hangman = {
+    wins: 0,
+    losses: 0,
+    pilotNames: ["Maverick", "Goose", "Iceman", "Hollywood", "Cougar", "Sunshine", "Viper", "Jester"] ,// user to guess pilot name
+    guessesRemaining: 12,
+    lettersGuessed: [],
+}
+
+// Game start function
+
+// Correct letter function
+
+// Incorrect letter function
+
+// Game win function
+
+// Game lose function
+
+// Create event key that does the following: Adds key to an array, checks if letter pressed is contained in the string that was randomly selected from an array. 
